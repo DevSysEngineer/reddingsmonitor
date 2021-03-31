274f96162c8134facd5450cc088e3a6af3dfd08f
Kevin van Steijn <info@kvansteijn.nl>
2021-03-31 21:43:11 +0200

    Improved upload

--- www/scripts/upload.php
+++ www/scripts/upload.php
@@ -2,6 +2,8 @@
 
 namespace Reddingsmonitor;
 
+use DOMDocument;
+
 /* Set dir */
 chdir(__DIR__);
 
@@ -10,7 +12,7 @@ try {
     require_once 'load.php';
 
     /* Stop here if we don't have any file */
-    if (empty($_FILES) || empty($_Files['kml'])) {
+    if (empty($_FILES) || empty($_FILES['kml'])) {
         header('HTTP/1.0 400 Bad Request');
         exit;
     }
@@ -26,8 +28,24 @@ try {
         exit;
     }
 
+    /* Try to load file */
+    $dom = new DOMDocument;
+    if (!$dom->load($_FILES['kml']['tmp_name'])) {
+        exit('FAILED');
+    }
+
+    /* Check if we have elements */
+    $elements = $dom->getElementsByTagName('Document');
+    if (empty($elements)) {
+        exit('FAILED');
+    }
+
     /* Move file */
-    move_uploaded_file($_FILES['kml']['tmp_name'], $config->getKMLLocation());
+    if (move_uploaded_file($_FILES['kml']['tmp_name'], $config->getKMLLocation())) {
+        exit('OK'); 
+    } else {
+        exit('FAILED');
+    }
 } catch (\Exception $e) {
     header('HTTP/1.0 500 Internal Server Error');
     exit;
