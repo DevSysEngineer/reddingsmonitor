31c67ff8739152f54409b2ccab40c8ebefd7d534
Kevin van Steijn <info@kvansteijn.nl>
2021-04-04 12:37:36 +0200

    Fixed other syntax error

--- javascripts/main.js
+++ javascripts/main.js
@@ -757,7 +757,7 @@ function loadRemoteData() {
             }
         };
         xhttp.send();
-    }.then(values => {
+    }).then(values => {
         // Get element by id and remove old childs
         var selectElement = document.getElementById('select-follow-mode');
         while (selectElement.firstChild) {
