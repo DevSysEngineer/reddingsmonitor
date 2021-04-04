0e322c2702d2c55b80292975e8ee852bf4c8a5aa
Kevin van Steijn <info@kvansteijn.nl>
2021-04-04 09:14:02 +0200

    Reset styling

--- www/index.php
+++ www/index.php
@@ -55,7 +55,9 @@ try {
                 <div id="logo">Reddings<span>monitor</span></div>
                 <div id="dark-mode">
                     <div id="lastupdate"></div>
-                    <div id="follow"><span>Follow</span><select id="select-follow-mode" onchange="triggerFollowMode()" /></div>
+                    <div id="follow">
+                        <span>Follow</span><select id="select-follow-mode" onchange="triggerFollowMode()" />
+                    </div>
                     <input id="checkbox-dark-mode" type="checkbox" value="1" onchange="triggerDarkMode(this.checked)" />
                     <label for="checkbox-dark-mode">Dark mode</label>
                 </div>
